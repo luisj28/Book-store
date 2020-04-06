c72460ab6e8f3990cfe03780435cb1b07b1b3626
Luis Gonzalez <Kay@localhost.localdomain>
2020-04-05 19:18:28 -0500

    mi primer commit

---
+++ src/config/config.service.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import { parse } from 'dotenv';
+
+export class ConfigService {
+  private readonly envConfing: { [key: string]: string };
+  constructor() {
+    const isDevelopmentEnv = process.env.NODE_ENV !== 'production';
+    if (isDevelopmentEnv) {
+      const envFilePath = __dirname + '/../../.env';
+      const existsPath = fs.existsSync(envFilePath);
+      if (existsPath) {
+        console.log('.env file doesnt not exist  ');
+        process.exit(0);
+      }
+      this.envConfing = parse(fs.readFileSync(envFilePath));
+    } else {
+      this.envConfing = {
+        PORT: process.env.PORT,
+      };
+    }
+  }
+  get(key: string): string {
+    return this.envConfing[key];
+  }
+}
